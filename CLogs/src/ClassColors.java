9c615a6e5aca7e89d95249f19c3dac8396c18898
Marcel Fortmann <marcelfortmann@googlemail.com>
2012-06-26 16:27:16 +0200

    farb-action, assignment 9

--- CLogs/src/ClassColors.java
+++ CLogs/src/ClassColors.java
@@ -6,98 +6,104 @@ public class ClassColors {
 	HashMap<String,Color> ccolor = new HashMap<String,Color> ();
 	
 	public ClassColors () {
-	// Klassenfarben	
-	ccolor.put("Miracelwip", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Spatzenhirn", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Illiash", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Cursia", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Kr�mml", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Troublemaker", 	new Color(1f,0.96f,0.41f));
-	ccolor.put("Kangee", 		new Color(0.77f,0.12f,0.23f));
-	ccolor.put("Delraius", 		new Color(0.58f,0.51f,0.79f));
-	ccolor.put("Fellmuh", 		new Color(0f,0.44f,0.87f));
-	ccolor.put("Madhorn", 		new Color(0f,0.44f,0.87f));
-	ccolor.put("Recall", 		new Color(1f,0.9f,0.8f));
+	// Klassenfarben													//H,S,V
+	ccolor.put("Miracelwip", 			new Color(1f,0.49f,0.04f)); 	//28,96,100
+	ccolor.put("Spatzenhirn", 			new Color(0.67f,0.83f,0.45f));  //85,46,83
+	ccolor.put("Illiash", 				new Color(0.78f,0.61f,0.43f));	//30,45,78
+	ccolor.put("Cursia", 				new Color(0.41f,0.8f,0.94f));	//200,59,100
+	ccolor.put("Kr�mml", 				new Color(0.96f,0.55f,0.73f)); 	//334,43,96
+	ccolor.put("Troublemaker", 			new Color(1f,0.96f,0.41f));		//56,59,100
+	ccolor.put("Kangee", 				new Color(0.77f,0.12f,0.23f));	//333,84,77
+	ccolor.put("Delraius", 				new Color(0.58f,0.51f,0.79f));	//255,35,79
+	ccolor.put("Fellmuh", 				new Color(0f,0.44f,0.87f));		//210,100,87
+	ccolor.put("Madhorn", 				new Color(0f,0.44f,0.87f));		//210,100,87
+	ccolor.put("Recall", 				new Color(1f,0.9f,0.8f));		//31,20,100
 	
-	ccolor.put("Attack", 	new Color(0f,0.44f,0.87f));
-	
-	ccolor.put("Wrath", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Starfire", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Insect Swarm", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Starsurge", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Starfall", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Moonfire", 	new Color(1f,0.96f,0.41f));
-	ccolor.put("Sunfire", 		new Color(0.77f,0.12f,0.23f));
-	
-	ccolor.put("Auto Shot", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Chimera Shot", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Steady Shot", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Wild Quiver", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Aimed Shot", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Arcane Shot", 	new Color(1f,0.96f,0.41f));
-	
-	ccolor.put("Fireball", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Ignite", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Pyroblast", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Combustion", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Living Bomb", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Flame Orb", 	new Color(1f,0.96f,0.41f));
+	Color c0 = new Color(0f,0f,0f);
+	Color c1 = new Color(1f,0f,0f);
+	Color c2 = new Color(0f,1f,0f);
+	Color c3 = new Color(0f,0f,1f);
+	Color c4 = new Color(1f,1f,0f);
+	Color c5 = new Color(0f,1f,1f);
+	Color c6 = new Color(1f,0f,1f);
 
-	ccolor.put("Templar's Verdict", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Hand of Light", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Crusader Strike", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Censure", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Seal of Truth", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Judgement of Truth", 	new Color(1f,0.96f,0.41f));
+	ccolor.put("Attack", 				c0);
+	ccolor.put("Auto Shot", 			c0);
 	
-	ccolor.put("Mind Flay", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Mind Blast", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Vampiric Touch", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Shadow Word: Pain", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Devouring Plague", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Shadow Word: Plague", 	new Color(1f,0.96f,0.41f));
+	ccolor.put("Wrath", 				c1); 
+	ccolor.put("Starfire", 				c2); 
+	ccolor.put("Insect Swarm", 			c3); 
+	ccolor.put("Starsurge", 			c4); 
+	ccolor.put("Starfall", 				c5); 
+	ccolor.put("Moonfire", 				c6);
+	
+	ccolor.put("Chimera Shot", 			c1);
+	ccolor.put("Steady Shot", 			c2);
+	ccolor.put("Wild Quiver", 			c3);
+	ccolor.put("Aimed Shot", 			c4);
+	ccolor.put("Arcane Shot", 			c5);
+	
+	ccolor.put("Fireball", 				c1);
+	ccolor.put("Ignite", 				c2);
+	ccolor.put("Pyroblast", 			c3);
+	ccolor.put("Combustion", 			c4);
+	ccolor.put("Living Bomb", 			c5);
+	ccolor.put("Flame Orb", 			c6);
 
-	ccolor.put("Sinister Strike", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Instant Poison", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Main Gauche", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Deadly Poison", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Eviscerate", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Rupture", 	new Color(1f,0.96f,0.41f));
+	ccolor.put("Templar's Verdict",		c1);
+	ccolor.put("Hand of Light", 		c2);
+	ccolor.put("Crusader Strike", 		c3);
+	ccolor.put("Censure", 				c4);
+	ccolor.put("Seal of Truth", 		c5);
+	ccolor.put("Judgement of Truth",	c6);
 	
-	ccolor.put("Death Coil", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Scourge Strike", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Festering Strike", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Blood Plague", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Frost Fever", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Death and Decay", 	new Color(1f,0.96f,0.41f));
-	
-	ccolor.put("Unstable Affliction", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Shadow Bolt", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Corruption", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Drain Soul", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Bane of Doom", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Haunt", 	new Color(1f,0.96f,0.41f));
-	
-	ccolor.put("Lava Lash", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Flametongue Attack", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Windfury Attack", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Stormstrike", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Lightning Bolt", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Flame Shock", 	new Color(1f,0.96f,0.41f));
+	ccolor.put("Mind Flay", 			c1);
+	ccolor.put("Mind Blast", 			c2);
+	ccolor.put("Vampiric Touch", 		c3);
+	ccolor.put("Shadow Word: Pain", 	c4);
+	ccolor.put("Devouring Plague", 		c5);
+	ccolor.put("Shadow Word: Plague",	c6);
 
-	ccolor.put("Heroic Strike", 	new Color(1f,0.49f,0.04f));
-	ccolor.put("Shield Slam", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Cleave", 		new Color(0.78f,0.61f,0.43f));
-	ccolor.put("Starsurge", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Revenge", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Deep Wounds", 	new Color(1f,0.96f,0.41f));
-	ccolor.put("Thunderclap", 		new Color(0.77f,0.12f,0.23f));
+	ccolor.put("Sinister Strike", 		c1);
+	ccolor.put("Instant Poison", 		c2);
+	ccolor.put("Main Gauche", 			c3);
+	ccolor.put("Deadly Poison", 		c4);
+	ccolor.put("Eviscerate", 			c5);
+	ccolor.put("Rupture", 				c6);
+	
+	ccolor.put("Death Coil", 			c1);
+	ccolor.put("Scourge Strike", 		c2);
+	ccolor.put("Festering Strike", 		c3);
+	ccolor.put("Blood Plague", 			c4);
+	ccolor.put("Frost Fever", 			c5);
+	ccolor.put("Death and Decay", 		c6);
+	
+	ccolor.put("Unstable Affliction",	c1);
+	ccolor.put("Shadow Bolt", 			c2);
+	ccolor.put("Corruption", 			c3);
+	ccolor.put("Drain Soul", 			c4);
+	ccolor.put("Bane of Doom", 			c5);
+	ccolor.put("Haunt", 				c6);
+	
+	ccolor.put("Lava Lash", 			c1);
+	ccolor.put("Flametongue Attack", 	c2);
+	ccolor.put("Windfury Attack", 		c3);
+	ccolor.put("Stormstrike", 			c4);
+	ccolor.put("Lightning Bolt", 		c5);
+	ccolor.put("Flame Shock", 			c6);
 
-	ccolor.put("Bloodthirst", 	new Color(0.67f,0.83f,0.45f));
-	ccolor.put("Devastate", 		new Color(0.41f,0.8f,0.94f));
-	ccolor.put("Whirlwind Maw", 		new Color(0.96f,0.55f,0.73f));
-	ccolor.put("Raging Blow", 	new Color(1f,0.96f,0.41f));
-	ccolor.put("Whirlwind", 		new Color(0.77f,0.12f,0.23f));
+	ccolor.put("Heroic Strike", 		c1);
+	ccolor.put("Shield Slam", 			c2);
+	ccolor.put("Cleave", 				c3);
+	ccolor.put("Revenge", 				c4);
+	ccolor.put("Deep Wounds", 			c5);
+	ccolor.put("Thunderclap", 			c6);
+
+	ccolor.put("Bloodthirst", 			c2);
+	ccolor.put("Devastate", 			c3);
+	ccolor.put("Whirlwind Maw", 		c4);
+	ccolor.put("Raging Blow", 			c5);
+	ccolor.put("Whirlwind", 			c6);
 	
 	}
 	
